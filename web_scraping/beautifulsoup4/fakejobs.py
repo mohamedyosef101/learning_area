764dc7d68295d2937b889136557a6d0d2ca89d4c
Mohamed Yosef <mohamedyosef101@outlook.com>
2023-12-24 20:29:41 +0100

    Extract data to csv
    I still have some issues with the csv file because it doesn't look like table

--- web_scraping/beautifulsoup4/fakejobs.py
+++ web_scraping/beautifulsoup4/fakejobs.py
@@ -6,6 +6,7 @@
 
 import requests as rq
 from bs4 import BeautifulSoup as bs
+import csv
 
 # go to fake jobs page
 url = "https://realpython.github.io/fake-jobs/"
@@ -20,18 +21,28 @@ results = soup.find(id="ResultsContainer")
 # Find elements by class name
 job_elements = results.find_all("div", class_="card-content")
 
+# Create a list to store job data
+jobs_data = []
+
 # Take a look at all of the elements
 for job_element in job_elements:
     title = job_element.find("h2", class_="title")
     company = job_element.find("h3", class_="company")
     location = job_element.find("p", class_="location")
 
-    # view all of it
-    print(f"""------------------
-        \nTitle: {title.text}
-        \nCompany: {company.text}
-        \nLocation: {location.text}
-""")
+    # Append the details to a list
+    jobs_data.append([title.text, company.text, location.text])
 
 # NOW run the file in the terminal by typing: 
 # python fakejobs.py
+    
+# SAVE the outcomes
+file = "jobs_data.csv"
+with open(file, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    # Writer Headers
+    writer.writerow(['title', 'company', 'location'])
+    # Add the other rows
+    writer.writerows(jobs_data)
+
+print(f"Data has been save to 'jobs_data.csv'")
